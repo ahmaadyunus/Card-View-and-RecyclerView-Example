96fa57732825ea9d6e8f23e842777dcc3845f3b1
ahmaadyunus <scootyunus@gmail.com>
2016-12-21 15:19:25 +0700

    First Commit

---
+++ app/src/main/java/com/example/ahmaadyunus/cardviewexample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.ahmaadyunus.cardviewexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView rv_product;
+    private List<Book> bookList = new ArrayList<>();
+    private BookAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rv_product = (RecyclerView) findViewById(R.id.rv_product);
+        mAdapter = new BookAdapter(bookList);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
+        rv_product.setLayoutManager(mLayoutManager);
+        rv_product.setItemAnimator(new DefaultItemAnimator());
+        rv_product.setAdapter(mAdapter);
+
+        initBookData();
+        rv_product.addOnItemTouchListener(new RecyclerViewTouchListener(getApplicationContext(), rv_product, new RecyclerViewClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), bookList.get(position).getTitle() + " is clicked!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getApplicationContext(), bookList.get(position).getTitle() + " is long pressed!", Toast.LENGTH_SHORT).show();
+
+            }
+        }));
+    }
+    private void initBookData() {
+        Book book = new Book("Hello Android", "Ed Burnette");
+        bookList.add(book);
+
+        book = new Book("Beginning Android 3", "Mark Murphy");
+        bookList.add(book);
+
+        book = new Book("Unlocking Android", "W. Frank Ableson");
+        bookList.add(book);
+
+        book = new Book("Android Tablet Development", "Wei Meng Lee");
+        bookList.add(book);
+
+        book = new Book("Android Apps Security", "Sheran Gunasekera");
+        bookList.add(book);
+
+        mAdapter.notifyDataSetChanged();
+    }
+}
